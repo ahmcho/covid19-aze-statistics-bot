42181147e1340ef1a6162b31763ef10811eb7419
ahmcho <mail@ahmcho.com>
2021-02-21 19:22:12 +0400

    Add timeout for message send

--- utils.js
+++ utils.js
@@ -57,7 +57,9 @@ const downloadPdf = (day, ctx, data, pool, type) => {
                     if (error) {
                         throw error;
                     }
-                    type === 'extended' ? ctx.replyWithPhoto(url, { caption: `${data.message}${data.additionalData}#koronavirus` }) : ctx.replyWithPhoto(url, { caption: `${data.message}#koronavirus` });
+                    setTimeout(() => {
+                        type === 'extended' ? ctx.replyWithPhoto(url, { caption: `${data.message}${data.additionalData}#koronavirus` }) : ctx.replyWithPhoto(url, { caption: `${data.message}#koronavirus` });
+                    },500);
                 }) : null
             });
         })
