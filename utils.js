529433e4140fc3279a478e98126dc9c05db5fca6
ahmcho <mail@ahmcho.com>
2021-02-21 19:25:46 +0400

    Fixed settimeout

--- utils.js
+++ utils.js
@@ -59,7 +59,7 @@ const downloadPdf = (day, ctx, data, pool, type) => {
                     }
                     setTimeout(() => {
                         type === 'extended' ? ctx.replyWithPhoto(url, { caption: `${data.message}${data.additionalData}#koronavirus` }) : ctx.replyWithPhoto(url, { caption: `${data.message}#koronavirus` });
-                    },500);
+                    },1000);
                 }) : null
             });
         })
